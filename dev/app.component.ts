1f402bd3e5758be4e69c1a915bb1e42ee7330b7c
Matt Lunalover <mlunalover@kabbage.com>
2016-07-06 11:03:46 -0400

    setup complete

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
+import {PuzzleComponent} from "./puzzle/puzzle.component";
 
 @Component({
     selector: 'app',
     template: `
-        
-    `
+        <my-puzzle></my-puzzle>
+    `,
+    directives: [PuzzleComponent]
 })
 export class AppComponent {
 
