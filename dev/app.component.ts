61ab686871c83e31c47fcc0bc4f03a5e37157eb9
Matt Lunalover <mlunalover@kabbage.com>
2016-07-08 15:17:15 -0400

    Revert "setup"
    This reverts commit cebb36e94c12fe5bf227522f49f391c5cc274e51.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
+import {PuzzleComponent} from "./puzzle/puzzle.component";
 
 @Component({
     selector: 'app',
     template: `
-        
+        <my-puzzle></my-puzzle>
     `,
-    directives: []
+    directives: [PuzzleComponent]
 })
 export class AppComponent {
 
